03bd11bab67528e034458e94dd3715d3b00c802b
Alamin <alaminprogramerr@gmail.com>
2021-03-29 19:13:48 +0600

    added  config

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    secretOrKey: 'secretkey',
+    // mongoURI: 'mongodb://localhost/bernard-kpi',
+    mongoURI: 'mongodb+srv://user:user@mern.a77ou.mongodb.net/akh-kpi?retryWrites=true&w=majority',
+}
